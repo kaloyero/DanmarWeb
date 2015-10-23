314c032ab2557e510a6e53de54d54c6878b52436
Jandro <kaloye_ale@hotmail.com>
2015-10-23 07:36:50 -0300

    se agrega mapping en VersionController para probar listado de json

--- src/main/java/com/facturador/danmar/controller/VersionController.java
+++ src/main/java/com/facturador/danmar/controller/VersionController.java
@@ -6,12 +6,15 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.facturador.danmar.model.Kiosko;
+
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class VersionController {
@@ -47,5 +50,27 @@ public class VersionController {
 
 		return model;
 
+	}
+	
+	@RequestMapping(value = "/getTestListDos", method = RequestMethod.GET)
+	public @ResponseBody List<Kiosko> obtenerListado(Model model,HttpServletRequest request) {
+	   
+		List<Kiosko> kioskos = new ArrayList<Kiosko>();
+	
+		Kiosko k1 = new Kiosko(12,"k1","prueba 1");
+		Kiosko k2 = new Kiosko(13,"k2","prueba 2");
+		Kiosko k3 = new Kiosko(14,"k3","prueba 3");
+		Kiosko k4 = new Kiosko(15,"k4","prueba 4");
+		Kiosko k5 = new Kiosko(16,"k5","prueba 5");
+		Kiosko k6 = new Kiosko(17,"k6","prueba 6");
+	
+		kioskos.add(k1);
+		kioskos.add(k2);
+		kioskos.add(k3);
+		kioskos.add(k4);
+		kioskos.add(k5);
+		kioskos.add(k6);
+
+	   return kioskos ;
 	}
 }
